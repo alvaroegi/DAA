950cf1240bfdde7240808befda56207b1581bbf6
alvaroegi <alvaro.egido2@gmail.com>
2023-03-23 23:45:04 +0100

    23-03

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 import sys
-import win32crypt
+
 
 # Press Mayús+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
@@ -102,3 +102,118 @@ def escribir(s):
 
 escribir("Hola")
 
+def sumatorio(m,n,g):
+    if(m == n):
+        return g(n)
+    else:
+        return g(m) + sumatorio(m+1,n,g)
+
+def sumatorio2(m,n,g):
+    if(n == m):
+        return g(m)
+    else:
+        return sumatorio2(m,n-1,g) + g(n)
+
+def sumatorio3(m,n,g):
+    mitad = (n+m) // 2
+    if (m == n):
+        return g(m)
+    else:
+        return sumatorio3(m,mitad,g) + sumatorio3(mitad+1,n,g)
+def g(x):
+    return x**2
+
+print(sumatorio(3,6,g))
+print(sumatorio2(3,6,g))
+print(sumatorio3(3,6,g))
+
+def containsNumber(n,a):
+    if(n == a):
+        return True
+    elif(n % 10 == a):
+        return True
+    elif (n // 10 == 0):
+        return False
+    else:
+        return containsNumber(n // 10,a)
+
+print(containsNumber(132237540982507329875435,9))
+
+def cambioBase(n,b):
+    if(n // b == 0):
+        return n
+    else:
+        return  10*cambioBase(n // b,b) + (n%b)
+
+print(cambioBase(142,5))
+
+def polinomio(n,p):
+    lon = len(p)
+    if(lon == 1):
+        return p[0]
+    else:
+        return p[lon-1]*(n)**(lon-1) + polinomio(n,p[:-1])
+
+def polinomio2(n,p):
+    lon = len(p)
+    if(lon == 1):
+        return p[0]
+    else:
+        return p[0] + n*polinomio2(n,p[1:])
+
+print(polinomio(3,[2,-4,5]))
+print(polinomio2(3,[2,-4,5]))
+
+def busLineal(n,p):
+    l = len(p)
+    if(l == 0):
+        return -1
+    elif(p[0] == n):
+        return 1
+    else:
+        return 1 + busLineal(n,p[1:])
+
+print(busLineal(9,[3,4,5,2,1,2,2,2,9]))
+
+def busOrdenada(n,p):
+    l = len(p)
+    if(l == 0) or (n < p[0]):
+        return -1
+    elif(p[0] == n):
+        return 1
+    else:
+        return 1 + busOrdenada(n,p[1:])
+
+print(busOrdenada(9,[3,4,5,2,1,2,2,2,9]))
+
+def subirEsc(n):
+    if (n == 1):
+        return 1
+    elif (n == 2):
+        return 2
+    else:
+        return subirEsc(n-1) + subirEsc(n-2)
+
+print(subirEsc(5))
+
+def Manhattan(x,y):
+    if (x == 0) or (y == 0):
+        return 1
+    else:
+        return Manhattan(x-1,y) + Manhattan(x,y-1)
+
+print(Manhattan(3,3))
+
+def biseccion(a,b,f,e):
+    mitad = (a+b)/2
+    if (f(mitad) == 0) or (b-a <= 2*e):
+        return mitad
+    elif (f(a)*f(b) < 0):
+        return biseccion(a,mitad,f,e)
+    else:
+        return biseccion(mitad,b,f,e)
+
+def f(x):
+    return x-2
+
+print(biseccion(1,2,f,0.001))
